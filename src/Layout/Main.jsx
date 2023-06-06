19f56771b7f9c163f0f42a09e3e43b8ef2d90808
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-06-07 02:20:57 +0600

    Footer component  added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 
 const Main = () => {
     return (
         <div>
             <Navbar />
-            <Outlet />
+            <div className='min-h-[calc(100vh-136px)]'>
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     </>
     return (
         <div>
-            <div className="navbar bg-[#555555] text-white">
+            <div className="navbar bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
